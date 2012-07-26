fc4bd978d53194b210f06eb1137bef3854a4df36
Kim Altintop <kim.altintop@gmail.com>
2012-07-26 11:59:43 +0200

    Make flatten a function, so we can apply the formatting last

--- src/main/java/org/vafer/jmx/pipe/ConverterPipe.java
+++ src/main/java/org/vafer/jmx/pipe/ConverterPipe.java
@@ -8,6 +8,7 @@ import org.vafer.jmx.output.Output;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -37,25 +38,35 @@ public final class ConverterPipe implements JmxPipe {
     }
 
     public void output(String node, JmxQuery.JmxAttribute metric) throws IOException, InstanceNotFoundException, AttributeNotFoundException, ReflectionException, MBeanException {
-        final String attribute = formatter.attributeName(metric.getBeanName(), metric.getAttributeName());
         try {
-            final Object value = metric.getAttributeValue();
-            flatten(node, prefix + attribute, value);
+            final Map<String,Number> metrics = flatten(
+                new HashMap<String,Number>(),
+                metric.getAttributeName(),
+                metric.getAttributeValue()
+            );
+
+            for (Map.Entry<String,Number> entry : metrics.entrySet()) {
+                output.output(
+                    node,
+                    formatter.attributeName(metric.getBeanName(), entry.getKey()),
+                    entry.getValue()
+                );
+            }
         } catch(Exception e) {
-            System.err.println(String.format("Failed to read attribute %s [%s]", attribute, e.getMessage()));
+            System.err.println(String.format("Failed to read attribute %s [%s]", metric.getAttributeName(), e.getMessage()));
         }
     }
 
-    private void flatten(String node, String attribute, Object value) throws IOException {
+    private Map<String,Number> flatten(Map<String,Number> acc, String attribute, Object value) throws IOException {
         if (value instanceof Number) {
 
-            output.output(node, attribute, (Number) value);
+            acc.put(attribute, (Number) value);
 
         } else if (value instanceof String) {
 
             final Number v = enums.resolve(attribute, (String) value);
             if (v != null) {
-                output.output(node, attribute, v);
+                acc.put(attribute, v);
             } else {
                 System.err.println(String.format("Missing enum for attribute %s [%s]", attribute, value));
             }
@@ -63,25 +74,25 @@ public final class ConverterPipe implements JmxPipe {
         } else if (value instanceof Set) {
 
             final Set set = (Set) value;
-            flatten(node, attribute + "__size", set.size());
+            acc.put(attribute + "__size", set.size());
             for(Object entry : set) {
-                flatten(node, attribute + "___" + entry, 1);
+                acc.put(attribute + "___" + entry, 1);
             }
 
         } else if (value instanceof List) {
 
             final List list = (List)value;
-            flatten(node, attribute + "__size", list.size());
+            acc.put(attribute + "__size", list.size());
             for(int i = 0; i<list.size(); i++) {
-                flatten(node, attribute + "___" + i, list.get(i));
+                flatten(acc, attribute + "___" + i, list.get(i));
             }
 
         } else if (value instanceof Map) {
 
             final Map<?,?> map = (Map<?,?>) value;
-            flatten(node, attribute + "__size", map.size());
+            acc.put(attribute + "__size", map.size());
             for(Map.Entry<?, ?> entry : map.entrySet()) {
-                flatten(node, attribute + "___" + entry.getKey(), entry.getValue());
+                flatten(acc, attribute + "___" + entry.getKey(), entry.getValue());
             }
 
         } else if (value instanceof CompositeData) {
@@ -89,14 +100,16 @@ public final class ConverterPipe implements JmxPipe {
             CompositeData composite = (CompositeData) value;
             CompositeType type = composite.getCompositeType();
             TreeSet<String> keysSet = new TreeSet<String>(type.keySet());
-            flatten(node, attribute + "__size", keysSet.size());
+            acc.put(attribute + "__size", keysSet.size());
             for(String key : keysSet) {
-                flatten(node, attribute + "___" + key, composite.get(key));
+                flatten(acc, attribute + "___" + key, composite.get(key));
             }
 
         } else {
             System.err.println(String.format("Failed to convert [%s]", attribute));
         }
+
+        return acc;
     }
 
     public void close() throws IOException {
